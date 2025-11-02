654b62405fd7b30084a969bc751fee056d0dd02a
owner <owner@DESKTOP-SGQ9V7V>
2025-11-02 12:42:54 +0900

    シンハライトの色変更

--- bnscup2025/terrain/terrain.cpp
+++ bnscup2025/terrain/terrain.cpp
@@ -100,7 +100,7 @@ Vec2 Terrain::Pushback(const Circle& circle) const {
 }
 
 Optional<Vec2> Terrain::CalcLineCollisionPoint(const Vec2& from, const Vec2& direction, double max_distance) const {
-  const Line ray{ from, from + direction * max_distance };
+  const Line ray { from, from + direction * max_distance };
 
   // 次にグリッド線と交差するtを求める関数
   auto GetNextT = [&](double current_t) -> double {
@@ -119,7 +119,7 @@ Optional<Vec2> Terrain::CalcLineCollisionPoint(const Vec2& from, const Vec2& dir
         const double dist_to_prev_grid = frac + epsilon;
         return current_t + dist_to_prev_grid / -velocity;
       }
-      };
+    };
 
     const Vec2 current_pos = from + direction * max_distance * current_t;
     const Optional<double> next_t_x = GetNextTSub(current_pos.x, direction.x * max_distance);
@@ -131,18 +131,18 @@ Optional<Vec2> Terrain::CalcLineCollisionPoint(const Vec2& from, const Vec2& dir
     else {
       return *next_t_x;
     }
-    };
+  };
 
   double t = 0.0;  // 媒介変数
   while (t <= 1.0) {
-    const Point cell_pos{
+    const Point cell_pos {
       static_cast<int>(std::floor((from.x + direction.x * max_distance * t))),
       static_cast<int>(std::floor((from.y + direction.y * max_distance * t)))
     };
     const auto lines = marching_squares_.GetEdgeLines().Get(cell_pos);
     for (const auto& line_array : lines) {
       for (size_t i = 0; i < line_array.size() - 1; ++i) {
-        const Line line{ line_array[i], line_array[i + 1] };
+        const Line line { line_array[i], line_array[i + 1] };
         const Optional<Vec2> intersection = ray.intersectsAt(line);
         if (intersection) {
           return intersection;
@@ -168,7 +168,7 @@ void Terrain::DigAt(const Vec2& center, double radius, double center_might, doub
   for (int y = y_min; y <= y_max; ++y) {
     for (int x = x_min; x <= x_max; ++x) {
       // ノード位置と中心からの距離を計算
-      const Point node_pos{ x, y };
+      const Point node_pos { x, y };
       const double dist = center.distanceFrom(node_pos);
       if (not node_grid_.IsBound(node_pos)) continue;
       if (dist > radius) continue;
@@ -181,7 +181,7 @@ void Terrain::DigAt(const Vec2& center, double radius, double center_might, doub
       double current_value = node_grid_.Get(node_pos).density;
       current_value += dig_might;
       current_value = std::clamp(current_value, 0.0, 1.0);
-      node_grid_.Set(node_pos, NodeInfo{ .density = current_value, .material = node_grid_.Get(node_pos).material });
+      node_grid_.Set(node_pos, NodeInfo { .density = current_value, .material = node_grid_.Get(node_pos).material });
 
       updated_node_.Set(node_pos, true);
       updated_node_.Set(node_pos.movedBy(-1, 0), true);
@@ -196,30 +196,30 @@ void Terrain::DigAt(const Vec2& center, double radius, double center_might, doub
 void Terrain::RenderGround(const Array<Point>& visible_cells, const camera::Camera& cam) const {
 
   {
-    const ScopedRenderStates2D blend{ render::BlendMode::AlphaMax() };
+    const ScopedRenderStates2D blend { render::BlendMode::AlphaMax() };
     const auto t = cam.CreateRenderTransformer();
 
     const auto& polygons = marching_squares_.GetPolygons();
     for (const auto& pos : visible_cells) {
       const auto cell_polygons = polygons.Get(pos);
       for (const auto& polygon : cell_polygons) {
-        polygon.draw(ColorF{ 0.1, 0.1, 0.30 });
+        polygon.draw(ColorF { 0.1, 0.1, 0.30 });
       }
 
       // デバッグ用: アクセスマップの表示
       if (false) {
         if (access_map_.GetAccessableMap().Get(pos)) {
-          Circle{ pos, 0.1 }.draw(ColorF{ 1, 0, 0 });
+          Circle { pos, 0.1 }.draw(ColorF { 1, 0, 0 });
 
           const auto direction = access_map_.GetDirectionMap().Get(pos);
-          if (direction.up) Line{ Vec2{pos}, Vec2{pos}.movedBy(0, -0.4) }.draw(0.1, ColorF{ 1, 0, 0 });
-          if (direction.down) Line{ Vec2{pos}, Vec2{pos}.movedBy(0, 0.4) }.draw(0.1, ColorF{ 1, 0, 0 });
-          if (direction.left) Line{ Vec2{pos}, Vec2{pos}.movedBy(-0.4, 0) }.draw(0.1, ColorF{ 1, 0, 0 });
-          if (direction.right) Line{ Vec2{pos}, Vec2{pos}.movedBy(0.4, 0) }.draw(0.1, ColorF{ 1, 0, 0 });
-          if (direction.up_left) Line{ Vec2{pos}, Vec2{pos}.movedBy(-0.4, -0.4) }.draw(0.1, ColorF{ 1, 0, 0 });
-          if (direction.up_right) Line{ Vec2{pos}, Vec2{pos}.movedBy(0.4, -0.4) }.draw(0.1, ColorF{ 1, 0, 0 });
-          if (direction.down_left) Line{ Vec2{pos}, Vec2{pos}.movedBy(-0.4, 0.4) }.draw(0.1, ColorF{ 1, 0, 0 });
-          if (direction.down_right) Line{ Vec2{pos}, Vec2{pos}.movedBy(0.4, 0.4) }.draw(0.1, ColorF{ 1, 0, 0 });
+          if (direction.up) Line { Vec2{pos}, Vec2{pos}.movedBy(0, -0.4) }.draw(0.1, ColorF { 1, 0, 0 });
+          if (direction.down) Line { Vec2{pos}, Vec2{pos}.movedBy(0, 0.4) }.draw(0.1, ColorF { 1, 0, 0 });
+          if (direction.left) Line { Vec2{pos}, Vec2{pos}.movedBy(-0.4, 0) }.draw(0.1, ColorF { 1, 0, 0 });
+          if (direction.right) Line { Vec2{pos}, Vec2{pos}.movedBy(0.4, 0) }.draw(0.1, ColorF { 1, 0, 0 });
+          if (direction.up_left) Line { Vec2{pos}, Vec2{pos}.movedBy(-0.4, -0.4) }.draw(0.1, ColorF { 1, 0, 0 });
+          if (direction.up_right) Line { Vec2{pos}, Vec2{pos}.movedBy(0.4, -0.4) }.draw(0.1, ColorF { 1, 0, 0 });
+          if (direction.down_left) Line { Vec2{pos}, Vec2{pos}.movedBy(-0.4, 0.4) }.draw(0.1, ColorF { 1, 0, 0 });
+          if (direction.down_right) Line { Vec2{pos}, Vec2{pos}.movedBy(0.4, 0.4) }.draw(0.1, ColorF { 1, 0, 0 });
 
         }
       }
@@ -235,14 +235,14 @@ void Terrain::RenderGroundEdges(const Array<Point>& visible_cells, const camera:
   {
     const auto target = lightbloom.CreateRenderTarget();
     const auto transform = cam.CreateRenderTransformer();
-    const auto blend{ render::BlendMode::AlphaMax() };
+    const auto blend { render::BlendMode::AlphaMax() };
 
     const auto& lines = marching_squares_.GetEdgeLines();
     for (const auto& pos : visible_cells) {
       const auto edge_lines = lines.Get(pos);
       for (const auto& vec_array : edge_lines) {
         for (size_t i = 0; i < vec_array.size() - 1; ++i) {
-          const Line line{ vec_array[i], vec_array[i + 1] };
+          const Line line { vec_array[i], vec_array[i + 1] };
           line.draw(LineStyle::RoundCap, 0.8, CalcEdgeColor(line.begin), CalcEdgeColor(line.end));
         }
       }
@@ -258,7 +258,7 @@ ColorF Terrain::CalcEdgeColor(const Vec2& pos) const {
   int y_i = static_cast<int>(std::floor(pos.y));
   double x_frac = pos.x - x_i;
   double y_frac = pos.y - y_i;
-  const Point cell_pos{ x_i, y_i };
+  const Point cell_pos { x_i, y_i };
   const auto color_lt = material_table_.at(node_grid_.Get(cell_pos.movedBy(0, 0)).material)->GetBaseColor();
   const auto color_rt = material_table_.at(node_grid_.Get(cell_pos.movedBy(1, 0)).material)->GetBaseColor();
   const auto color_lb = material_table_.at(node_grid_.Get(cell_pos.movedBy(0, 1)).material)->GetBaseColor();
@@ -286,7 +286,7 @@ ColorF Terrain::CalcEdgeColor(const Vec2& pos) const {
   const double int_rb = coef_rb * material_table_.at(node_grid_.Get(cell_pos.movedBy(1, 1)).material)->GetSinhaliteEffectiveness();
   const double sinhalite_effectiveness = int_lt + int_rt + int_lb + int_rb;
 
-  constexpr ColorF kSinhaliteColor{ 0.20, 0.15, 0.01 };
+  constexpr ColorF kSinhaliteColor { 0.20, 0.20, 0.01 };
   return color_base.lerp(kSinhaliteColor, EaseInCubic(sinhalite_intensity * sinhalite_effectiveness));
 }
 
@@ -318,7 +318,7 @@ Array<Point> Terrain::CreateVisibleCells(const camera::Camera& cam) const {
 
   index_count_x = std::min(index_count_x, static_cast<int>(GetCellGridSize().x) - begin_index_x);
   index_count_y = std::min(index_count_y, static_cast<int>(GetCellGridSize().y) - begin_index_y);
-  Array<Point> visible_cells{ Arg::reserve = index_count_y * index_count_x };
+  Array<Point> visible_cells { Arg::reserve = index_count_y * index_count_x };
   for (int y = 0; y <= index_count_y; ++y) {
     for (int x = 0; x < index_count_x; ++x) {
       visible_cells.emplace_back(begin_index_x + x, begin_index_y + y);
